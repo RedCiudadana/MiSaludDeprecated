e0a3a6a305fb451c49c5e63d541654b1239709af
D3AH <diegoeauyonh@gmail.com>
2018-11-12 13:14:46 -0600

    Configurando lanzamiento.

--- config/deploy.js
+++ config/deploy.js
@@ -22,9 +22,9 @@ module.exports = function(deployTarget) {
 
     // configure other plugins for production deploy target here
     ENV.git = {
-      repo: 'https://github.com/RedCiudadana/MiGuatemala.git',
+      repo: 'https://github.com/RedCiudadana/MiSalud.git',
       branch: 'gh-pages',
-      worktreePath: '/tmp/deploy-miguatemala'
+      worktreePath: '/tmp/deploy-misalud'
     };
   }
 
